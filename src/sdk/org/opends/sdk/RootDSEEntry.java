5078cdee446d60108043a9e3c9b65b143c758a4a
matthew_swift <matthew_swift@41b1ffd8-f28e-4786-ab96-9950f0a78031>
2009-09-11 09:33:59 +0000

    Move RootDSEEntry into correct package.
    git-svn-id: https://svn.forgerock.org/opendj/branches/data-providers@5813 41b1ffd8-f28e-4786-ab96-9950f0a78031

--- src/sdk/org/opends/sdk/RootDSEEntry.java
+++ src/sdk/org/opends/sdk/RootDSEEntry.java
@@ -1,17 +1,47 @@
-import org.opends.sdk.*;
-import org.opends.sdk.util.Validator;
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE
+ * or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at
+ * trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
+ * add the following below this CDDL HEADER, with the fields enclosed
+ * by brackets "[]" replaced with your own identifying information:
+ *      Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ *
+ *      Copyright 2009 Sun Microsystems, Inc.
+ */
+
+package org.opends.sdk;
+
+
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.opends.sdk.schema.Schema;
+import org.opends.sdk.util.Validator;
 import org.opends.server.types.ByteString;
 
-import java.util.*;
-import java.net.URI;
-
 /**
- * Created by IntelliJ IDEA.
- * User: boli
- * Date: Sep 9, 2009
- * Time: 2:18:10 PM
- * To change this template use File | Settings | File Templates.
+ * Root DSE Entry.
  */
 public class RootDSEEntry extends AbstractEntry
 {
